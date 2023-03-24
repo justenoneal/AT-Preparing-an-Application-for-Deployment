22394a5f133ce208a2a72e3291d7ee6eda49c333
justenoneal <justenoneal@gmail.com>
2023-03-23 20:38:51 -0700

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -15,11 +15,11 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
-  "production": {
-    "username": process.env.RDS_USERNAME,
-    "password": process.env.RDS_PASSWORD,
-    "database": process.env.RDS_DB_NAME,
-    "host": process.env.RDS_HOSTNAME,
-    "port": process.env.RDS_PORT,
-    "dialect": "postgres"
-  }
+"production": {
+  "username": process.env.RDS_USERNAME,
+  "password": process.env.RDS_PASSWORD,
+  "database": process.env.RDS_DB_NAME,
+  "host": process.env.RDS_HOSTNAME,
+  "port": process.env.RDS_PORT,
+  "dialect": "postgres"
+}
